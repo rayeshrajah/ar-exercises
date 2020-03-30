b19312e1c572e3176a80b243edd2201a361a5878
Rayesh <rayeshrajah@gmail.com>
2020-03-30 17:39:06 -0400

    Added destroy table row for stores table

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+
+@store3.destroy
+
+puts "After Removing and Active Record we get #{Store.count} stores"
