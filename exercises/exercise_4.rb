c7046d25038c094a8c0d120b2bb739ec1d34ff50
Rayesh <rayeshrajah@gmail.com>
2020-03-30 18:40:50 -0400

    Added necessary queries for exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,34 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+surrey = Store.create(
+  name: "Surray",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true 
+)
+
+whistler = Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true 
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+puts "Men Only Clothing Stores with name and annual revenue"
+@mens_stores.each {|store| puts "Store: #{store.name} \n Annual Revenue: #{store.annual_revenue}\n\n"}
+
+
+ @womens_stores = Store.where("womens_apparel = ? AND annual_revenue < ?", true, 1000000)
+
+ puts "Women Only Clothing Stores with name and annual revenue less than 1000000"
+ @womens_stores.each {|store| puts "Store: #{store.name} \n Annual Revenue: #{store.annual_revenue}\n\n"}
